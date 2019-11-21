fecc0a2136972aa70535ffdb2c0bea840bb2bb37
Indy Naessens <indy.naessens@student.ucll.be>
2019-11-21 17:00:59 +0100

    dns_add_record - CNAME works

--- scripts/dns_zone_helper.py
+++ scripts/dns_zone_helper.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+### THIS SCRIPT ONLY SUPPORTS DOMAINS CREATED BY THE SCRIPT LOCATED AT:
+### /etc/scripts/dns_add_zone
+
 # imports
 from dnszone import dnszone
 import sys
@@ -8,6 +11,15 @@ def handle_a(subdomain, ip, domain):
   zone = dnszone.zone_from_file(domain, '/etc/bind/zones/db.mrt.' + domain)
   zone.add_name(subdomain + '.' + domain + '.')
   zone.names[subdomain + '.' +  domain + '.'].records('A', create=True).add(ip)
+
+  zone.save(autoserial=True)
+
+def handle_cname(alias, host_domain):
+  domain = '.'.join(host_domain.split('.')[1:-1]) + '.be'
+
+  zone = dnszone.zone_from_file(domain, '/etc/bind/zones/db.mrt.' + domain)
+  zone.add_name(host_domain + '.')
+  zone.names[host_domain + '.'].records('CNAME', create=True).add(alias + '.' + domain)
 
   zone.save(autoserial=True)
 
@@ -24,7 +36,10 @@ if arguments[0] == 'A':
 elif arguments[0] == 'MX':
   print('MX')
 elif arguments[0] == 'CNAME':
-  print('CNAME')
+  alias = str(arguments[1])
+  host_domain = str(arguments[2])
+
+  handle_cname(alias, host_domain)
 else:
   print('Wrong argument provided, accepts: A, MX, CNAME')
   sys.exit(0)
