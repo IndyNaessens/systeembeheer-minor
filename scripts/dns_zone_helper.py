8d8166814c16d0ea7a262254ff4555050e726543
Indy Naessens <indy.naessens@student.ucll.be>
2019-11-21 17:42:41 +0100

    dns_add_record - python script finished

--- scripts/dns_zone_helper.py
+++ scripts/dns_zone_helper.py
@@ -18,8 +18,14 @@ def handle_cname(alias, host_domain):
   domain = '.'.join(host_domain.split('.')[1:-1]) + '.be'
 
   zone = dnszone.zone_from_file(domain, '/etc/bind/zones/db.mrt.' + domain)
-  zone.add_name(host_domain + '.')
-  zone.names[host_domain + '.'].records('CNAME', create=True).add(alias + '.' + domain)
+  zone.add_name(alias + '.' + domain + '.')
+  zone.names[alias + '.' + domain + '.'].records('CNAME', create=True).add(host_domain + '.')
+
+  zone.save(autoserial=True)
+
+def handle_mx(mx_name, ip, domain):
+  zone = dnszone.zone_from_file(domain, '/etc/bind/zones/db.mrt.' + domain)
+  zone.names[domain + '.'].records('MX', create=True).add((10, mx_name))
 
   zone.save(autoserial=True)
 
@@ -28,13 +34,18 @@ arguments = sys.argv[1:]
 
 # handle request based on first argument
 if arguments[0] == 'A':
-  subdomain = arguments[1]
-  ip = arguments[2]
-  domain = arguments[3]
+  subdomain = str(arguments[1])
+  ip = str(arguments[2])
+  domain = str(arguments[3])
 
-  handle_a(str(subdomain), str(ip), str(domain))
+  handle_a(subdomain, ip, domain)
 elif arguments[0] == 'MX':
-  print('MX')
+  mx_name = str(arguments[1])
+  ip = str(arguments[2])
+  domain = str(arguments[3])
+
+  handle_a(mx_name, ip, domain)
+  handle_mx(mx_name, ip, domain)
 elif arguments[0] == 'CNAME':
   alias = str(arguments[1])
   host_domain = str(arguments[2])
