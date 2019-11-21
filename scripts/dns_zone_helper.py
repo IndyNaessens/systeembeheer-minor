17a91ab41ade21002167aac36733fdc02075e70c
Indy Naessens <indy.naessens@student.ucll.be>
2019-11-21 16:43:27 +0100

    dns_add_record - A record working

--- scripts/dns_zone_helper.py
+++ scripts/dns_zone_helper.py
@@ -4,19 +4,27 @@
 from dnszone import dnszone
 import sys
 
+def handle_a(subdomain, ip, domain):
+  zone = dnszone.zone_from_file(domain, '/etc/bind/zones/db.mrt.' + domain)
+  zone.add_name(subdomain + '.' + domain + '.')
+  zone.names[subdomain + '.' +  domain + '.'].records('A', create=True).add(ip)
+
+  zone.save(autoserial=True)
+
+# get needed arguments
 arguments = sys.argv[1:]
 
-if arguments[0] == 'NS':
-  print('NS')
-elif arguments[0] == 'A':
-  print('A')
+# handle request based on first argument
+if arguments[0] == 'A':
+  subdomain = arguments[1]
+  ip = arguments[2]
+  domain = arguments[3]
+
+  handle_a(str(subdomain), str(ip), str(domain))
 elif arguments[0] == 'MX':
   print('MX')
 elif arguments[0] == 'CNAME':
   print('CNAME')
 else:
-  print('Wrong argument provided, accepts: NS, A, MX, CNAME')
+  print('Wrong argument provided, accepts: A, MX, CNAME')
   sys.exit(0)
-
-# zone = dnszone.zone_from_file('indy-naessens.sb.uclllabs.be', '/etc/bind/zones/db.indy-naessens.sb.uclllabs.be')
-
